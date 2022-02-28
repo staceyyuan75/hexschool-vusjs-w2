f8ebb5744e7c71fb5d93e9725ddfd3a9364a7d34
Stacey Yuan <staceyyuan75@gmail.com>
2022-02-28 21:40:02 +0800

    增加送出表單前，檢查帳號密碼是否有輸入

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue
 createApp({
   data() {
     return {
-      apiUrl: "https://vue3-course-api.hexschool.io/v2"
+      apiUrl: "https://vue3-course-api.hexschool.io/v2",
+      isValid: {
+        username: true,
+        password: true
+      }
     };
   },
   methods: {
@@ -15,24 +19,33 @@ createApp({
         username,
         password
       };
-      axios
-        .post(url, user)
-        .then(res => {
-          const { token, expired } = res.data;
-          document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
-          window.location.href = "./index.html";
-        })
-        .catch(error => {
-          let isSuccess = error.data.success;
-          let message = error.data.message;
-          if (!isSuccess) {
-            Swal.fire({
-              icon: "error",
-              title: message,
-              text: "請重新登入"
-            });
-          }
-        });
+
+      this.isValid.username =
+        username == "" || username == undefined ? false : true;
+      this.isValid.password =
+        password == "" || password == undefined ? false : true;
+
+      if (username != "" && password != "") {
+        axios
+          .post(url, user)
+          .then(res => {
+            const { token, expired } = res.data;
+            document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+            window.location.href = "./index.html";
+          })
+          .catch(error => {
+            let isSuccess = error.data.success;
+            let message = error.data.message;
+            console.log(error.data);
+            if (!isSuccess) {
+              Swal.fire({
+                icon: "error",
+                title: message,
+                text: "請重新登入"
+              });
+            }
+          });
+      }
     }
   }
 }).mount("#app");
